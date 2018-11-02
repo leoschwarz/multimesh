9833f48e6c3daafb76b25c817aad04b5f40011be
Leonardo Schwarz <mail@leoschwarz.com>
2018-11-02 10:07:13 +0100

    more docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
+//! # Overview
+//! The goal of this crate is to provide a generic interface for serializing and
+//! deserializing geometric meshes into various formats.
+//!
+//! ## Status
+//! The API is still in draft state, since it's necessary to find abstractions
+//! which are suitable for a wide range of formats while still being efficient and
+//! ergonomic for real world and big data set usage.
+
 extern crate nalgebra;
 extern crate regex;
 #[macro_use]
